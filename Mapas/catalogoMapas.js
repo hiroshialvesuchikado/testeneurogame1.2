8d40508d615c17f905dd7aeb1a39d1d9aa523f38
hiroshialvesuchikado <hiroshialvesuchikado@gmail.com>
2026-09-11 00:58:05 -0300

    Update catalogoMapas.js

--- Mapas/catalogoMapas.js
+++ Mapas/catalogoMapas.js
@@ -1,122 +1,42 @@
-// ======================================================
-// CATÁLOGO DE MAPAS — NEUROGAME
-// ======================================================
-
-
-// ======================================================
-// VERIFICAÇÃO DOS MAPAS CARREGADOS
-// ======================================================
-
-console.log(
-    "Lateral:",
-    typeof mapaTelencefaloLateral
-);
-
-console.log(
-    "Medial:",
-    typeof mapaTelencefaloMedial
-);
-
-console.log(
-    "Cadáver lateral:",
-    typeof mapaCadaverSulcosTelencefaloLateral
-);
-
-
-// ======================================================
-// CATÁLOGO
-// ======================================================
-
 const catalogoMapas = [
 
-    // ==================================================
-    // TELENCÉFALO — VISTA LATERAL
-    // ==================================================
-
     {
-        id:
-            "telencefalo-lateral",
+        id: "telencefalo-lateral",
 
         titulo:
             "Telencéfalo — Vista Lateral",
 
         imagem:
-            "../imagens/PalcoTelencefalo.png",
+            "imagens/PalcoTelencefalo.png",
 
         estruturas:
             mapaTelencefaloLateral
     },
 
-
-    // ==================================================
-    // TELENCÉFALO — VISTA MEDIAL
-    // ==================================================
-
     {
-        id:
-            "telencefalo-medial",
+        id: "telencefalo-medial",
 
         titulo:
             "Telencéfalo — Vista Medial",
 
         imagem:
-            "../imagens/PalcoTelencefaloMedial.png",
+            "imagens/PalcoTelencefaloMedial.png",
 
         estruturas:
             mapaTelencefaloMedial
     },
 
-
-    // ==================================================
-    // CADÁVER — SULCOS TELENCÉFALO LATERAL
-    // ==================================================
-
     {
-        id:
-            "cadavertelencefalo-lateral",
+        id: "cadavertelencefalo-lateral",
 
         titulo:
             "Sulcos Telencéfalo — Vista Lateral",
 
         imagem:
-            "../imagens/CadaverTelencefaloLateral.png",
+            "imagens/CadaverTelencefaloLateral.png",
 
         estruturas:
             mapaCadaverSulcosTelencefaloLateral
     }
 
 ];
-
-
-// ======================================================
-// VERIFICAÇÕES
-// ======================================================
-
-console.log(
-    "Catálogo carregado:",
-    catalogoMapas
-);
-
-
-console.log(
-    "Qtd lateral:",
-    mapaTelencefaloLateral.length
-);
-
-
-console.log(
-    "Qtd medial:",
-    mapaTelencefaloMedial.length
-);
-
-
-console.log(
-    "Qtd cadáver lateral:",
-    mapaCadaverSulcosTelencefaloLateral.length
-);
-
-
-console.log(
-    "Qtd de mapas no catálogo:",
-    catalogoMapas.length
-);
